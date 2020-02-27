694f30d21f88c9ad92d0c083a0cc5441b93c468d
tangxin <2417219614@qq.com>
2020-02-27 14:11:36 +0800

    重新实现一个轮播，让无缝轮播和点击能同时实现。其中还是有BUG的

--- main.js
+++ main.js
@@ -1,103 +1,72 @@
-// 一个想法：一个一个移动
-// setTimeout(function(){
-//     $('.images>img:nth-child(1)').css({
-//         transform: 'translateX(-100%)'
-//     })
-//     $('.images>img:nth-child(2)').css({
-//         transform: 'translateX(-100%)'
-//     })
-//     $('.images>img:nth-child(1)').one('transitionend',function(e){
-//         $(e.currentTarget).addClass('right').css({transform: 'none'})
-//     })
-// }, 3000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-// setTimeout(function(){
-//     $('.images>img:nth-child(2)').css({
-//         transform: 'translateX(-200%)'
-//     })
-//     $('.images>img:nth-child(3)').css({
-//         transform: 'translateX(-100%)'
-//     })
-//     $('.images>img:nth-child(2)').one('transitioned',function(e){
-//         $(e.currentTarget).addClass('right').css({transform: 'none'})
-//     })
-// }, 6000)
+makeFakeSlides()
+$slides.css({transform: 'translateX(-400px)'})
+bindEvents()
 
-// setTimeout(function(){
-//     $('.images>img:nth-child(3)').css({
-//         transform: 'translateX(-200%)'
-//     })
-//     $('.images>img:nth-child(1)').css({
-//         transform: 'translateX(-100%)'
-//     })
-//     $('.images>img:nth-child(3)').one('transitioned',function(e){
-//         $(e.currentTarget).addClass('right').css({transform: 'none'})
-//     })
-// }, 9000)
-
-// 我的想法
-// 另一个想法，都使用绝对定位，这样各个图片之间就不存在位置联系，可以单独给各个图片设样式
-// 需要一个容器装样式，是不变的样式，这个样式容器将布局分成三个部分，第一部分的left，第二部分是middle，第三部分是right
-// 由于会出现覆盖的情况，我们需要给各个图片添加z-index，始终左边的z-index设成最小
-cssArray = initialize($('.images>img'))
-var length = $('.images>img').length
-var n = 1
-let timer = setInterval(function(){
-    for(let i=0;i<length;i++){
-        $('.images>img').eq((n+i)%(length)).removeClass("leave current enter").addClass(cssArray[i])
-    }
-    n += 1
-},1000)
-
-// 为防止切换网页的时候出现BUG，我们加以下代码，来规定切换的时候停住
-document.addEventListener('visibilitychange', function(e){
-    if(document.hidden){
-        window.clearInterval(timer)
-    }else{
-        timer = setInterval(function(){
-            for(let i=0;i<length;i++){
-                $('.images>img').eq((n+i)%(length)).removeClass("leave current enter").addClass(cssArray[i])
-            }
-            n += 1
-        },1000)
-    }
+// 上一页下一页
+$('#next').on('click', function(){
+    goToSlide(current+1)
+})
+$('#previous').on('click', function(){
+    goToSlide(current-1)
 })
 
-// 设置初始化状态以及生成样式列表，这个样式列表会在image之间进行传递型循环
-function initialize(nodes){
-    var cssArray = []
-    for(let i=0;i<nodes.length;i++){
-        nodes.eq(i).addClass('enter')
-        cssArray[i] = 'enter'
-    }
-    nodes.eq(0).removeClass('enter').addClass('current')
-    cssArray[0] = 'current'
-    cssArray[nodes.length-1] = 'leave'
-    return cssArray
+// 自动播放
+let timer = setInterval(function(){
+    goToSlide(current + 1)
+},2000)
+$('.container').on('mouseenter', function(){
+    window.clearInterval(timer)
+}).on('mouseleave', function(){
+    timer = setInterval(function(){
+        goToSlide(current + 1)
+    },2000)
+})
+
+function bindEvents(){
+    $('#buttonWrapper').on('click', 'button', function(e){
+        // 这边这种监听方式使用的是监听的是click事件，对象是buttonWrapper，但是点击buttonWrapper没用，点击它里面的button才有用
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
 }
 
+// 这个函数比较重要，我们拥有了去到任何一张图片的可能
+function goToSlide(index){
+    if(index>$buttons.length - 1){
+        index = 0
+    }else if(index < 0){
+        index = $buttons.length - 1
+    }
+    if(current === $buttons.length - 1 && index === 0){
+        // 最后一张到第一张
+        $slides.css({transform: `translateX(${-($buttons.length + 1) *400}px)`})
+            .one('transitionend', function(){
+                $slides.hide().offset()
+                $slides.css({transform: `translateX(${-(index+1)*400}px)`}).show()
+            })
+    }else if(current === 0 && index === $buttons.length - 1){
+        // 第一张到最后一张
+        $slides.css({transform: 'translateX(0px)'})
+            .one('transitionend', function(){
+                $slides.hide().offset()
+                $slides.css({transform: `translateX(${-(index+1)*400}px)`}).show()
+            })
+    }else{
+        $slides.css({transform: `translateX(${- (index+1) *400}px)`})
+    }
+    current = index
+}
 
-// 老师的想法
-// 有三种状态，是可以写成循环的，字符串里面
-// $('.images >img:nth-child(1)').addClass('current')
-// $('.images >img:nth-child(2)').addClass('enter')
-// $('.images >img:nth-child(3)').addClass('enter')
-// let n = 1
-// setInterval(()=>{
-//     $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-//         .one('transitionend', (e)=>{
-//             $(e.currentTarget).removeClass('leave').addClass('enter')
-//         })
-//     $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
-//     n += 1
-// },3000)
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
 
-// function x(n) {
-//     if(n>3){
-//         n = n%3
-//         if(n===0){
-//             n=3
-//         }
-//     }
-//     return n
-// }
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
